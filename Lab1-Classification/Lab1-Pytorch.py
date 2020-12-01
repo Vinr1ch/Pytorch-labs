0dc4a6f8e9706b0f77860c50c6fe8d4569783fa8
vinrich <colin@vinarcik.com>
2020-12-01 14:00:11 -0500

    update to code

--- Lab1-Classification/Lab1-Pytorch.py
+++ Lab1-Classification/Lab1-Pytorch.py
@@ -1,54 +1,270 @@
+from __future__ import print_function
+import os
+import numpy as np
+import random
+import argparse
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from sklearn.metrics import *
+from matplotlib import pyplot as plt
 
 
-class TwoLayerNet(torch.nn.Module):
-    def __init__(self, D_in, H, D_out):
-        """
-        In the constructor we instantiate two nn.Linear modules and assign them as
-        member variables.
-        """
-        super(TwoLayerNet, self).__init__()
-        self.linear1 = torch.nn.Linear(D_in, H)
-        self.linear2 = torch.nn.Linear(H, D_out)
+# Setting random seeds to keep everything deterministic.
+random.seed(1618)
+np.random.seed(1618)
+#tf.set_random_seed(1618)   # Uncomment for TF1.
+#tf.random.set_seed(1618)
+
+# Disable some troublesome logging.
+#tf.logging.set_verbosity(tf.logging.ERROR)   # Uncomment for TF1.
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# Information on dataset.
+NUM_CLASSES = 10
+IMAGE_SIZE = 784
+
+# Use these to set the algorithm to use.
+#ALGORITHM = "guesser"
+#ALGORITHM = "custom_net"
+ALGORITHM = "tf_net"
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        self.fc1 = nn.Linear(4*4*50, 500)
+        self.fc2 = nn.Linear(500, 10)
 
     def forward(self, x):
-        """
-        In the forward function we accept a Tensor of input data and we must return
-        a Tensor of output data. We can use Modules defined in the constructor as
-        well as arbitrary operators on Tensors.
-        """
-        h_relu = self.linear1(x).clamp(min=0)
-        y_pred = self.linear2(h_relu)
-        return y_pred
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2, 2)
+        x = x.view(-1, 4*4*50)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
 
 
-# N is batch size; D_in is input dimension;
-# H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = 64, 1000, 100, 10
 
-# Create random Tensors to hold inputs and outputs
-x = torch.randn(N, D_in)
-y = torch.randn(N, D_out)
+class NeuralNetwork_2Layer():
+    def __init__(self, inputSize, outputSize, neuronsPerLayer, learningRate = 0.1):
+        self.inputSize = inputSize
+        self.outputSize = outputSize
+        self.neuronsPerLayer = neuronsPerLayer
+        self.lr = learningRate
+        self.W1 = np.random.randn(self.inputSize, self.neuronsPerLayer)
+        self.W2 = np.random.randn(self.neuronsPerLayer, self.outputSize)
 
-# Construct our model by instantiating the class defined above
-model = TwoLayerNet(D_in, H, D_out)
+    # Activation function.
+    def __sigmoid(self, x):
+        pass   #TODO: implement
 
-# Construct our loss function and an Optimizer. The call to model.parameters()
-# in the SGD constructor will contain the learnable parameters of the two
-# nn.Linear modules which are members of the model.
-criterion = torch.nn.MSELoss(reduction='sum')
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
-for t in range(500):
-    # Forward pass: Compute predicted y by passing x to the model
-    y_pred = model(x)
+    # Activation prime function.
+    def __sigmoidDerivative(self, x):
+        pass   #TODO: implement
 
-    # Compute and print loss
-    loss = criterion(y_pred, y)
-    if t % 100 == 99:
-        print(t, loss.item())
+    # Batch generator for mini-batches. Not randomized.
+    def __batchGenerator(self, l, n):
+        for i in range(0, len(l), n):
+            yield l[i : i + n]
 
-    # Zero gradients, perform a backward pass, and update the weights.
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
-    #we will see
+    # Training with backpropagation.
+    def train(self, xVals, yVals, epochs = 100000, minibatches = True, mbs = 100):
+        pass                                   #TODO: Implement backprop. allow minibatches. mbs should specify the size of each minibatch.
+
+    # Forward pass.
+    def __forward(self, input):
+        layer1 = self.__sigmoid(np.dot(input, self.W1))
+        layer2 = self.__sigmoid(np.dot(layer1, self.W2))
+        return layer1, layer2
+
+    # Predict.
+    def predict(self, xVals):
+        _, layer2 = self.__forward(xVals)
+        return layer2
+
+
+
+# Classifier that just guesses the class label.
+def guesserClassifier(xTest):
+    ans = []
+    for entry in xTest:
+        pred = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        pred[random.randint(0, 9)] = 1
+        ans.append(pred)
+    return np.array(ans)
+
+#pytorch Functions
+
+
+def trainPy(model, device, train_loader, optimizer, epoch):
+    losses = []
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.item())
+        if batch_idx > 0 and batch_idx % 100 == 0:
+            print('Train Epoch: {} [{}/{}\t({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item()))
+    return losses
+def testPy(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    return (float(correct) / len(test_loader.dataset))
+
+def test_label_predictions(model, device, test_loader):
+    model.eval()
+    actuals = []
+    predictions = []
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            prediction = output.argmax(dim=1, keepdim=True)
+            actuals.extend(target.view_as(prediction))
+            predictions.extend(prediction)
+    return [i.item() for i in actuals], [i.item() for i in predictions]
+
+def test_label_predictions(model, device, test_loader):
+    model.eval()
+    actuals = []
+    predictions = []
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            prediction = output.argmax(dim=1, keepdim=True)
+            actuals.extend(target.view_as(prediction))
+            predictions.extend(prediction)
+    return [i.item() for i in actuals], [i.item() for i in predictions]
+
+#=========================<Pipeline Functions>==================================
+
+def getRawData():
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(
+            '../data',
+           train=True,
+           download=True,
+           transform=transforms.Compose([
+               transforms.ToTensor(),
+               transforms.Normalize((0.1307,), (0.3081,))
+           ])
+        ),
+        batch_size=64,
+        shuffle=True)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(
+            '../data',
+            train=False,
+            transform=transforms.Compose([
+               transforms.ToTensor(),
+               transforms.Normalize((0.1307,), (0.3081,))
+            ])
+        ),
+        batch_size=1000,
+        shuffle=True)
+    return (train_loader, test_loader)
+
+
+"""
+def preprocessData(raw):
+    ((xTrain, yTrain), (xTest, yTest)) = raw            #TODO: Add range reduction here (0-255 ==> 0.0-1.0).
+    yTrainP = to_categorical(yTrain, NUM_CLASSES)
+    yTestP = to_categorical(yTest, NUM_CLASSES)
+    print("New shape of xTrain dataset: %s." % str(xTrain.shape))
+    print("New shape of xTest dataset: %s." % str(xTest.shape))
+    print("New shape of yTrain dataset: %s." % str(yTrainP.shape))
+    print("New shape of yTest dataset: %s." % str(yTestP.shape))
+    return ((xTrain, yTrainP), (xTest, yTestP))
+
+"""
+
+
+
+def trainModel(data):
+    train_loader = data
+    if ALGORITHM == "guesser":
+        return None   # Guesser has no model, as it is just guessing.
+    elif ALGORITHM == "custom_net":
+        print("Building and training Custom_NN.")
+        print("Not yet implemented.")                   #TODO: Write code to build and train your custon neural net.
+        return None
+    elif ALGORITHM == "tf_net":
+        model = CNN()
+        optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+        device = torch.device("cpu") # or 'gpu'
+        losses = []
+        accuracies = []
+        for epoch in range(0, 1):
+            losses.extend(trainPy(model, device, train_loader, optimizer, epoch))
+            accuracies.append(testPy(model, device, train_loader))
+        return model
+    else:
+        raise ValueError("Algorithm not recognized.")
+
+
+
+def runModel(data, model):
+    if ALGORITHM == "guesser":
+        return guesserClassifier(data)
+    elif ALGORITHM == "custom_net":
+        print("Testing Custom_NN.")
+        print("Not yet implemented.")                   #TODO: Write code to run your custon neural net.
+        return None
+    elif ALGORITHM == "tf_net":
+        test_loader = data
+        print("Testing TF_NN.")
+        device = torch.device("cpu") # or 'gpu'
+        actuals, predictions = test_label_predictions(model, device, test_loader)
+        return actuals, predictions
+    else:
+        raise ValueError("Algorithm not recognized.")
+
+
+
+def evalResults(data, actuals, predictions):
+
+    print('Confusion matrix:')
+    print(confusion_matrix(actuals, predictions))
+    print('F1 score: %f' % f1_score(actuals, predictions, average='micro'))
+    print('Accuracy score: %f' % accuracy_score(actuals, predictions))
+
+
+
+#=========================<Main>================================================
+
+def main():
+    data = getRawData()
+    #data = preprocessData(raw)
+    model = trainModel(data[0])
+    actuals, predictions = runModel(data[1], model)
+    evalResults(data, actuals, predictions)
+
+
+
+if __name__ == '__main__':
+    main()
