f693a9ea7ff9e4cf64fb1c5ccdd806c698e79e2e
vinrich <colin@vinarcik.com>
2020-12-02 12:49:32 -0500

    final comments of lab 1

--- Lab1-Classification/Lab1-Pytorch.py
+++ Lab1-Classification/Lab1-Pytorch.py
@@ -10,11 +10,13 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from sklearn.metrics import *
 from matplotlib import pyplot as plt
-
+#resources used was pytroch api
+#https://pytorch.org/docs/stable/index.html
 
 # Setting random seeds to keep everything deterministic.
-random.seed(1618)
-np.random.seed(1618)
+#removed as it was unnneeded
+#random.seed(1618)
+#np.random.seed(1618)
 #tf.set_random_seed(1618)   # Uncomment for TF1.
 #tf.random.set_seed(1618)
 
@@ -29,7 +31,7 @@ IMAGE_SIZE = 784
 # Use these to set the algorithm to use.
 #ALGORITHM = "guesser"
 #ALGORITHM = "custom_net"
-ALGORITHM = "tf_net"
+ALGORITHM = "pytorch_NN"
 
 class CNN(nn.Module):
     def __init__(self):
@@ -102,60 +104,72 @@ def guesserClassifier(xTest):
 #pytorch Functions
 
 
-def trainPy(model, device, train_loader, optimizer, epoch):
-    losses = []
+def train_pytroch(model, device, train_loader, opt, epoch):
+    loss = []
     model.train()
+    #loop used for training
     for batch_idx, (data, target) in enumerate(train_loader):
+        #load data and target and change to device being used
         data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
+        #update optimizer with zero_grads
+        opt.zero_grad()
+        #run model with data
         output = model(data)
+        #calulate losses
         loss = F.nll_loss(output, target)
         loss.backward()
-        optimizer.step()
-        losses.append(loss.item())
+        opt.step()
+        #updates loss and append information
+        loss.append(loss.item())
+        #update imformation every 100 batches
         if batch_idx > 0 and batch_idx % 100 == 0:
             print('Train Epoch: {} [{}/{}\t({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
-    return losses
-def testPy(model, device, test_loader):
+    return loss
+
+
+def test_pytorch(model, device, test_loader):
+    #evalute the model
     model.eval()
-    test_loss = 0
-    correct = 0
+    #variable to hold number of correct and total total loss of program
+    t_l = 0
+    n_corr = 0
+
     with torch.no_grad():
+        #loop to run model on test set
         for data, target in test_loader:
+            #load data in varibales
             data, target = data.to(device), target.to(device)
+            #get output from model
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
-    test_loss /= len(test_loader.dataset)
+            # get total sum of batch loss
+            t_l += F.nll_loss(output, target, reduction='sum').item()
+            # get the index of log-probability
+            pred = output.argmax(dim=1, keepdim=True)
+            n_corr += pred.eq(target.view_as(pred)).sum().item()
+    t_l /= len(test_loader.dataset)
+    #output data to printed about average loss and accuracy
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
-    return (float(correct) / len(test_loader.dataset))
+        t_l, n_corr, len(test_loader.dataset),
+        100. * n_corr / len(test_loader.dataset)))
+    #return data to be used in output for evaluation
+    return (float(n_corr) / len(test_loader.dataset))
 
 def test_label_predictions(model, device, test_loader):
     model.eval()
+    #create arrays to hold values for confusion_matrix
     actuals = []
     predictions = []
-    with torch.no_grad():
-        for data, target in test_loader:
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-            prediction = output.argmax(dim=1, keepdim=True)
-            actuals.extend(target.view_as(prediction))
-            predictions.extend(prediction)
-    return [i.item() for i in actuals], [i.item() for i in predictions]
 
-def test_label_predictions(model, device, test_loader):
-    model.eval()
-    actuals = []
-    predictions = []
     with torch.no_grad():
+        #loop used to update data and get accuracy of model
         for data, target in test_loader:
+            #load data in varibales
             data, target = data.to(device), target.to(device)
+            #get output from model
             output = model(data)
+            # get the index of log-probability
             prediction = output.argmax(dim=1, keepdim=True)
             actuals.extend(target.view_as(prediction))
             predictions.extend(prediction)
@@ -164,6 +178,8 @@ def test_label_predictions(model, device, test_loader):
 #=========================<Pipeline Functions>==================================
 
 def getRawData():
+    #load datat from MNIST from pytorch
+    #Training set to true, also normalize data for use
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST(
             '../data',
@@ -191,6 +207,7 @@ def getRawData():
 
 
 """
+#removec as it is unneeded with program, should be changed for custom NN
 def preprocessData(raw):
     ((xTrain, yTrain), (xTest, yTest)) = raw            #TODO: Add range reduction here (0-255 ==> 0.0-1.0).
     yTrainP = to_categorical(yTrain, NUM_CLASSES)
@@ -213,15 +230,17 @@ def trainModel(data):
         print("Building and training Custom_NN.")
         print("Not yet implemented.")                   #TODO: Write code to build and train your custon neural net.
         return None
-    elif ALGORITHM == "tf_net":
+    elif ALGORITHM == "pytorch_NN":
         model = CNN()
-        optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
-        device = torch.device("cpu") # or 'gpu'
-        losses = []
-        accuracies = []
-        for epoch in range(0, 1):
-            losses.extend(trainPy(model, device, train_loader, optimizer, epoch))
-            accuracies.append(testPy(model, device, train_loader))
+        opt = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+        device = torch.device("cpu") # or 'gpu' can be changed as needed
+
+        #array used for outpur used later to analyze model
+        loss = []
+        we_right = []
+        for epoch in range(0, 5):
+            loss.extend(train_pytroch(model, device, train_loader, opt, epoch))
+            we_right.append(test_pytorch(model, device, train_loader))
         return model
     else:
         raise ValueError("Algorithm not recognized.")
@@ -235,10 +254,10 @@ def runModel(data, model):
         print("Testing Custom_NN.")
         print("Not yet implemented.")                   #TODO: Write code to run your custon neural net.
         return None
-    elif ALGORITHM == "tf_net":
+    elif ALGORITHM == "pytorch_NN":
         test_loader = data
         print("Testing TF_NN.")
-        device = torch.device("cpu") # or 'gpu'
+        device = torch.device("cpu") # or 'gpu' can be changed as needed
         actuals, predictions = test_label_predictions(model, device, test_loader)
         return actuals, predictions
     else:
@@ -247,11 +266,12 @@ def runModel(data, model):
 
 
 def evalResults(data, actuals, predictions):
-
-    print('Confusion matrix:')
-    print(confusion_matrix(actuals, predictions))
-    print('F1 score: %f' % f1_score(actuals, predictions, average='micro'))
-    print('Accuracy score: %f' % accuracy_score(actuals, predictions))
+    #this is use for confusion_matrix output, but will not workon custom NN
+    if ALGORITHM == "pytorch_NN":
+        print('Confusion matrix:')
+        print(confusion_matrix(actuals, predictions))
+        print('F1 score: %f' % f1_score(actuals, predictions, average='micro'))
+        print('Accuracy score: %f' % accuracy_score(actuals, predictions))
 
 
 
@@ -259,6 +279,7 @@ def evalResults(data, actuals, predictions):
 
 def main():
     data = getRawData()
+    #removed as it was unneeded from
     #data = preprocessData(raw)
     model = trainModel(data[0])
     actuals, predictions = runModel(data[1], model)
